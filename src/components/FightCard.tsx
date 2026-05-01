c8872f811a6b09b070f2ae7a965c6b043f27c645
Pedro Matos <pedrohsmatosprog@gmail.com>
2026-05-01 20:00:57 -0300

    v1.4.6

--- src/components/FightCard.tsx
+++ src/components/FightCard.tsx
@@ -1,3 +1,10 @@
+interface FightCardProps {
+  fighter1: string;
+  fighter2: string;
+  image: string;
+  colorBg: string;
+  corTexto: string;
+}
 
 const colors20 = {
   orange: "bg-orange-500/20",
@@ -5,6 +12,7 @@ const colors20 = {
   green: "bg-green-500/20",
   black: "bg-black/20",
 };
+
 const colors = {
   orange: "bg-orange-500/90",
   red: "bg-red-500/90",
@@ -12,29 +20,50 @@ const colors = {
   black: "bg-black/90",
 };
 
-export function FightCard({ fighter1, fighter2, image,colorBg,corTexto }) {
+export function FightCard({
+  fighter1,
+  fighter2,
+  image,
+  colorBg,
+  corTexto,
+}: FightCardProps) {
   return (
     <div
       className="group relative mx-auto overflow-hidden rounded-2xl border-2
                  h-[20vw] md:h-[10vw]
                  w-[85vw]
                  hover:h-[93vw]
-                 md:mt-6
-                mt-2
+                 md:mt-6 mt-2
                  md:w-[40vw]
                  md:hover:h-[50vw]
                  transition-all duration-300
                  border-orange-500 bg-cover bg-center
-                flex justify-center items-center bg-[10%_10%]"
+                 flex justify-center items-center bg-[10%_10%]"
       style={{ backgroundImage: `url(${image})` }}
     >
-      {/* overlay */}
-      <div  className={`absolute  inset-0 ${colors[colorBg]} md:group-hover:${colors20[colorBg]} group-hover:bg-${colorBg}/20  transition-all duration-300`}></div>
+      {/* overlay normal */}
+      <div
+        className={`absolute inset-0 transition-all duration-300
+          ${colors[colorBg as keyof typeof colors]}
+          group-hover:opacity-0
+        `}
+      ></div>
+
+      {/* overlay hover */}
+      <div
+        className={`absolute inset-0 opacity-0 transition-all duration-300
+          ${colors20[colorBg as keyof typeof colors20]}
+          group-hover:opacity-100
+        `}
+      ></div>
 
       {/* conteúdo */}
-      <div className="relative group-hover:opacity-0 z-10 flex flex-col justify-center h-full text-center text-white">
-        <h2 className="text-base sm:text-lg md:text-4xl font-bold px-2 " style={{color:corTexto}}>
-          {fighter1} <span className='text-amber-400'>vs</span> {fighter2}
+      <div className="relative group-hover:opacity-0 z-10 flex flex-col justify-center h-full text-center text-white transition-all duration-300">
+        <h2
+          className="text-base sm:text-lg md:text-4xl font-bold px-2"
+          style={{ color: corTexto }}
+        >
+          {fighter1} <span className="text-amber-400">vs</span> {fighter2}
         </h2>
       </div>
     </div>
